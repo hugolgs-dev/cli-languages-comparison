01b7df4ccfa35f20526ca8df706cf7bc794129c3
hugolgs-dev <hugo.ligneres@orange.fr>
2025-07-24 18:31:22 +0200

    add zig (WIP)

---
+++ zig/build.zig
@@ -0,0 +1,25 @@
+const Build = @import("std").Build;
+
+pub fn build(b: *Build) void {
+    const exe_mod           = b.createModule(.{
+        .root_source_file   = b.path("src/main.zig"),
+        .target             = b.standardTargetOptions(.{}),
+        .optimize           = b.standardOptimizeOption(.{}),
+    });
+
+    const exe               = b.addExecutable(.{
+        .name               = "cli",
+        .root_module        = exe_mod,
+    });
+
+    b.installArtifact(exe);
+
+    const exe_tests         = b.addTest(.{
+        .root_module        = exe_mod,
+    });
+
+    const run_exe_tests     = b.addRunArtifact(exe_tests);
+
+    const test_step         = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_tests.step);
+}
---
+++ zig/src/main.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const cli = @import("cli.zig");
+const cmd = @import("commands.zig");
+
+pub fn main() !void {
+    // Define available commands
+    const commands = [_]cli.command{
+        cli.command{
+            .name = "hello",
+            .func = &cmd.methods.commands.helloFn,
+            .opt = &.{"name"}, // "name" is optional for the hello command
+        },
+        cli.command{
+            .name = "help",
+            .func = &cmd.methods.commands.helpFn,
+        },
+
+        // add each new command here, inside commands
+    };
+
+    // Define available options
+    const options = [_]cli.option{
+        cli.option{
+            .name = "name",
+            .short = 'n',
+            .long = "name",
+            .func = &cmd.methods.options.nameFn,
+        },
+    };
+
+    // Start the CLI application
+    try cli.start(&commands, &options, true);
+}
